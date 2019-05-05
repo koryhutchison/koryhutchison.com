fbefab58cc30aa11611eda702963ca425b8acb8a
Kory Hutchison <kory.hutchison@icloud.com>
2019-05-04 20:57:02 -0600

    Set up project page with the 3 main projects

---
+++ src/components/kory-section.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="padding">
+        <div class="content-container pad-sides">
+            <div v-if="title" class="text-center">
+                <h1>{{ title }}</h1>
+            </div>
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: String
+    }
+}
+</script>
+
+<style scoped>
+.content-container {
+    max-width: 1070px;
+    margin: auto;
+}
+
+.padding {
+    padding-top: 60px;
+    padding-bottom: 60px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Navigation from './components/navigation'
 import BrandContainer from './components/brand-container'
+import KorySection from './components/kory-section'
 
 Vue.component('navigation', Navigation)
 Vue.component('brandContainer', BrandContainer)
+Vue.component('korySection', KorySection)
 
 const main = new Vue({
   el: '#main'

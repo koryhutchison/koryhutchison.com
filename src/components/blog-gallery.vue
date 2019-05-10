a1e8414e7b69e9956a4fbef7b9ce0bb729d382a4
Kory Hutchison <kory.hutchison@icloud.com>
2019-05-09 22:55:11 -0600

    Created blog-gallery component and set up json endpoints

---
+++ src/components/blog-gallery.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h1 v-for="cat in categories">{{ cat }}</h1>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    data () {
+        return {
+            categories: [],
+            posts: []
+        }
+    },
+    mounted () {
+        axios.get('/_data/categories.json')
+             .then(response => {
+                 this.categories = response.data;
+             });
+
+        axios.get('/_data/posts.json')
+             .then(response => {
+                 this.posts = response.data;
+             });
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import Navigation from './components/navigation'
 import BrandContainer from './components/brand-container'
 import KorySection from './components/kory-section'
+import BlogGallery from './components/blog-gallery'
 
 Vue.component('navigation', Navigation)
 Vue.component('brandContainer', BrandContainer)
 Vue.component('korySection', KorySection)
+Vue.component('blogGallery', BlogGallery)
 
 const main = new Vue({
   el: '#main'

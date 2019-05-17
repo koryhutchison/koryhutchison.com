583750f706d8c02e805b65456d7f64b173707b61
Kory Hutchison <kory.hutchison@domo.com>
2019-05-17 13:40:03 -0600

    Switched to only import the languages I want

--- src/main.js
+++ src/main.js
@@ -5,11 +5,23 @@ import KorySection from './components/kory-section'
 import BlogGallery from './components/blog-gallery'
 
 import VueHighlightJS from 'vue-highlight.js';
-import 'vue-highlight.js/lib/allLanguages'
+import vue from 'vue-highlight.js/lib/languages/vue';
+import python from 'highlight.js/lib/languages/python'
+import javascript from 'highlight.js/lib/languages/javascript'
+import bash from 'highlight.js/lib/languages/bash'
+import ruby from 'highlight.js/lib/languages/ruby'
 
 import 'highlight.js/styles/github.css';
 
-Vue.use(VueHighlightJS)
+Vue.use(VueHighlightJS, {
+    languages: {
+        vue,
+        python,
+        javascript,
+        bash,
+        ruby
+    }
+})
 
 Vue.component('navigation', Navigation)
 Vue.component('brandContainer', BrandContainer)
